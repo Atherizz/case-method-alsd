caf24f9c318cbda92b16db9c19307376f4a0a3a6
Atherizz <saveroathallah86@gmail.com>
2025-05-30 14:12:53 +0700

    first commit

---
+++ TransaksiLayanan.java
@@ -0,0 +1,18 @@
+public class TransaksiLayanan {
+    Pasien pasien;
+    Dokter dokter;
+    int durasiLayanan;
+    int biaya;
+
+    public TransaksiLayanan(Pasien pasien, Dokter dokter, int durasiLayanan) {
+        this.pasien = pasien;
+        this.dokter = dokter;
+        this.durasiLayanan = durasiLayanan;
+        this.biaya = hitungBiaya();
+    }
+
+    public int hitungBiaya() {
+        int tarifPerJam = 50000;
+        return durasiLayanan * tarifPerJam;
+    }
+}
